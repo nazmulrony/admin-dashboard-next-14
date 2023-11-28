1db4dbc1f154d383b081e9d2b65bea6d4a28f2eb
nazmulrony <nhrony65@gmail.com>
2023-11-28 22:16:51 +0600

    project initiated

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+
+type DashboardLayoutProps = {
+    children: ReactNode;
+};
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+    return <div>layout</div>;
+}
---
+++ src/components/dashboard/sidebar/sidebar.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function Sidebar() {
+    return <div>Sidebar</div>;
+}
