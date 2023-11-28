e063e72ebdb26a77db2496867ca996d5e4ba33a0
nazmulrony <nhrony65@gmail.com>
2023-11-28 22:18:16 +0600

    dashboard layout added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,20 @@
+import Navbar from "@/components/dashboard/navbar/Navbar";
+import Sidebar from "@/components/dashboard/sidebar/Sidebar";
 import { ReactNode } from "react";
 
 type DashboardLayoutProps = {
     children: ReactNode;
 };
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-    return <div>layout</div>;
+    return (
+        <div>
+            <div>
+                <Sidebar />
+            </div>
+            <div>
+                <Navbar />
+                {children}
+            </div>
+        </div>
+    );
 }
