5dbce0e570bfaf97b9bff2a6dc78a47b0af08669
nazmulrony <nhrony65@gmail.com>
2023-12-03 00:21:07 +0600

    add

--- src/components/dashboard/footer.tsx
+++ src/components/dashboard/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="flex items-center justify-between mt-8 text-textSoft">
-            <p className="font-bold">Nazmul Rony</p>
+            <p className="font-bold text-sm">Nazmul Rony</p>
             <p className="text-xs">© All rights reserved</p>
         </div>
     );
